b2a29699ab29ad846ced62d81fd000a7d33d8fbd
Luv Ratan <luvrtn2023@gmail.com>
2023-01-13 22:13:54 +0530

    cors signin signup router material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
-import BasicLayout from "./layouts/Layout";
+import Layout from "./components/Layout";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <BasicLayout />
+      <Layout />
+      <Routes>
+        <Route exact path="/login" element={<SignIn />} />
+        <Route exact path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
